fe1d9b713dadafc1a2dd9b120cf07db8f89d4881
khalsel <khaled.sellami.1@ulaval.ca>
2024-05-23 14:08:50 -0400

    build script

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+PROJ_PATH=$(pwd)
+
+
+for lang in "$@"
+do
+  if [ "$lang" = "go" ]
+  then
+    echo "Building Go parser executable"
+    cd "$PROJ_PATH"/go-service || exit 1
+    go build -o build/MicroAnalyzer
+  elif [ "$lang" = "java" ]
+  then
+    echo "Building Java parser jar file"
+    cd "$PROJ_PATH"/java-service || exit 1
+    mvn clean package
+  elif [ "$lang" = "c#" ]
+  then
+    echo "Building C# parser executable"
+    cd "$PROJ_PATH"/csharp-service/MicroAnalyzer || exit 1
+    dotnet publish -c Release -o build
+  elif [ "$lang" = "python" ]
+  then
+    echo "Installing Python parser package"
+    cd "$PROJ_PATH"/python-service || exit 1
+    pip install .
+  else
+    echo "Unknown language: "$lang""
+  fi
+done
+cd "$PROJ_PATH" || exit 1
+exit 0
